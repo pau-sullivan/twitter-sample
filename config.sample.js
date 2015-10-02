16509a0f83acac2f6ebf0c38d5ab545569925d2c
unknown <paulas@Paulas-PC.lar2.mobilestreams.com>
2015-10-02 16:08:10 -0300

    twitter

---
+++ config.sample.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+	twitter: {
+  	consumer_key: '9ZsrThZR9RJgiNgPvkKH2ftn5',
+		consumer_secret: 'Fcf5ROsvOamYCPcsE9stgroWzEVddmNIpCIdWEurvX0LTscION',
+		access_token: '148809395-u5Ibo66rhhY0Yb0TSOKC9hqPwOfTZuzopRPol3V3',
+		access_token_secret: '0Lhaj3fRn4JwT5WkX5FYdNw7gPgKosHG8pRu65Qka00Wx'
+	}
+
+};
